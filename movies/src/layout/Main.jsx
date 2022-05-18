a83138a03100a0fea51c4d3ca68325dd8b442a71
yavlash-liza <yavlashliza@gmail.com>
2022-05-18 19:53:50 +0300

    adding card with movies

--- movies/src/layout/Main.jsx
+++ movies/src/layout/Main.jsx
@@ -1,7 +1,35 @@
-function Main() {
-    return <main className="container content">
-        Hello from Movies App!
-    </main>
+import React from "react";
+import { Movies } from "../components/Movies";
+import { Search } from "../components/Search";
+import { Preloader } from "../components/Preloader";
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=c1e7901c&s=matrix")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=c1e7901c&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <main className="container content">
+        <Search searchMovies={this.searchMovies}/>
+        {movies.length ? <Movies movies={movies} /> : <Preloader />}
+      </main>
+    );
+  }
 }
 
-export {Main}
+export { Main };
