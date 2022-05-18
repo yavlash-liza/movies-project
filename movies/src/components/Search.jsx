39413067f55e4032633fd4ce2b165cf4714b02b9
yavlash-liza <yavlashliza@gmail.com>
2022-05-18 19:54:20 +0300

    adding search

---
+++ movies/src/components/Search.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Search extends React.Component {
+  state = {
+    search: "",
+  };
+
+  handleKey = (e) => {
+    if (e.key === "Enter") {
+      this.props.searchMovies(this.state.search);
+    }
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="input-field">
+          <input
+            className="validate"
+            placeholder="Search"
+            type="search"
+            value={this.state.search}
+            onChange={(e) => this.setState({ search: e.target.value })}
+            onKeyDown={this.handleKey}
+          />
+          <button className="btn search-btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Search };
